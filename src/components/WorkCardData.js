965d990feb8c28e0e83879a40860c81da6a082b6
Your Name <jakestonie@gmail.com>
2023-07-22 11:51:52 +0300

    still fixing url issues

--- src/components/WorkCardData.js
+++ src/components/WorkCardData.js
@@ -2,13 +2,15 @@ import mugirango from "../assets/mugirango.png";
 import mcbbys from "../assets/mcbbys.png";
 import restaurant from "../assets/restaurant.png";
 import bank from "../assets/banking.png";
-import store from "../assets/store.png"
+import store from "../assets/store.png";
+
+const rootURL = window.location.origin;
 
 const ProjectCardData = [
     {
         imgsrc: mugirango,
         title: "West Mugirango Official Website",
-        text: "TA social platform courtesy of West Mugirango's MP Stephen Mogaka. It allows residents of the said constituency to seemlessly access crucial social services remotely.",
+        text: "A social platform courtesy of West Mugirango's MP Stephen Mogaka. It allows residents of the said constituency to seemlessly access crucial social services & information remotely.",
         view: "https://w-mugirango.org/",
     },
 
@@ -16,28 +18,28 @@ const ProjectCardData = [
         imgsrc: mcbbys,
         title: "Retail Ecommerce Website Design",
         text: "Thriving Online shop where you'll find a delightful array of handcrafted soaps and candles, designed to infuse your spaces with captivating scents and soothing serenity.",
-        view: "/store_app/#2",
+        view: rootURL + "/store_app/#2",
     },
 
     {
         imgsrc: restaurant,
         title: "Fine Restaurant Website Design",
         text: "Made for the finest. Showcase extraordinary culinary delights, combine artful flavors, impeccable service, and a warm ambiance unite to create unforgettable dining experiences.",
-        view: "/restaurant_app/",
+        view: rootURL + "/restaurant_app/",
     },
 
     {
         imgsrc: bank,
         title: "Online banking service Website Design",
         text: "Empower your financial journey with our secure online banking service, offering seamless transactions, robust account management, and personalized solutions for your needs.",
-        view: "/banking_app/",
+        view: rootURL + "/banking_app/",
     },
 
     {
         imgsrc: store,
         title: "Next-gen Ecommerce Website Design",
         text: "Step into a world of boundless choices and convenience. From everyday essentials to unique finds, we curate an extensive collection to cater to all your needs.",
-        view: "/online_store/",
+        view: rootURL + "/online_store/",
     },
 ];
 
