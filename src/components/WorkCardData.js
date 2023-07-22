70b1ef6461ae68751f86194078013fc84de48892
Your Name <jakestonie@gmail.com>
2023-07-22 11:31:57 +0300

    fixed url problems

--- src/components/WorkCardData.js
+++ src/components/WorkCardData.js
@@ -16,28 +16,28 @@ const ProjectCardData = [
         imgsrc: mcbbys,
         title: "Retail Ecommerce Website Design",
         text: "Thriving Online shop where you'll find a delightful array of handcrafted soaps and candles, designed to infuse your spaces with captivating scents and soothing serenity.",
-        view: "https://toshkruno.github.io/store_app/#2",
+        view: "/store_app/#2",
     },
 
     {
         imgsrc: restaurant,
         title: "Fine Restaurant Website Design",
         text: "Made for the finest. Showcase extraordinary culinary delights, combine artful flavors, impeccable service, and a warm ambiance unite to create unforgettable dining experiences.",
-        view: "https://toshkruno.github.io/restaurant_app/",
+        view: "/restaurant_app/",
     },
 
     {
         imgsrc: bank,
         title: "Online banking service Website Design",
         text: "Empower your financial journey with our secure online banking service, offering seamless transactions, robust account management, and personalized solutions for your needs.",
-        view: "https://toshkruno.github.io/banking_app/",
+        view: "/banking_app/",
     },
 
     {
         imgsrc: store,
         title: "Next-gen Ecommerce Website Design",
         text: "Step into a world of boundless choices and convenience. From everyday essentials to unique finds, we curate an extensive collection to cater to all your needs.",
-        view: "https://toshkruno.github.io/online_store/",
+        view: "/online_store/",
     },
 ];
 
